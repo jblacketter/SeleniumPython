634ed6704586ef90287220f03223898d4ab9021f
Jack Blacketter <jblacketter@revelar.com>
2019-02-28 17:07:18 -0800

    updating the model

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,30 +1,38 @@
 import pytest
-import os
-import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from pages import login_page
+
 
 class TestLogin():
 
     @pytest.fixture
-    def driver(self, request):
+    def login(self, request):
         _geckodriver = os.path.join(os.getcwd(), 'vendor', 'geckodriver') 
         driver_ = webdriver.Firefox(executable_path=_geckodriver)
 
-        def quit():
+        def quit(): 
             driver_.quit()
 
         request.addfinalizer(quit)
-        return driver_
+        return login_page.LoginPage(driver_)
+
+    def test_valid_credentials(self, login): 
+        login.with_("revboomdoom@dispostable.com", "Welcome1@") 
+        assert login.success_message_present()
 
 
-    def test_valid_credentials(self, driver):
-       driver.get("https://revelar-qa-portal-practitioner.azurewebsites.net/login") 
-       time.sleep(3)
-       driver.find_element(By.ID, "login-email").send_keys("revboomdoom@dispostable.com") 
-       driver.find_element(By.ID, "login-password").send_keys("Welcome1@") 
-       driver.find_element(By.ID, "login-button").click()
-       time.sleep(1)
-       assert driver.find_element(By.ID, "navbar-btn-Dashboard").is_displayed()
+    def test_invalid_credentials(self, login): 
+        login.with_("tomsmith", "bad password")
+        assert login.success_message_present() == False
 
-       time.sleep(5)
+
+
+      # driver.find_element(By.ID, "login-email").send_keys("revboomdoom@dispostable.com") 
+       #driver.find_element(By.ID, "login-password").send_keys("Welcome1@") 
+       #driver.find_element(By.ID, "login-button").click()
+       #assert driver.find_element(By.ID, "navbar-btn-Dashboard").is_displayed()
+
+  #  def test_invalid_credentials(self, login): 
+    #    login.with_("tomsmith", "bad password") 
+
+    #    assert login.success_message_present() == False
